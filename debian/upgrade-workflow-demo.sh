344c31b0bb4916bc3df13500da148fee65b2906d
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2023-11-01 14:58:02 +0000

    [no ci] wip

--- debian/upgrade-workflow-demo.sh
+++ debian/upgrade-workflow-demo.sh
@@ -6,10 +6,18 @@ set -x
 
 rm -rf .build
 ./build ostreeRepo
-cp .build/ostreeRepo-${dpkg --print-architecture}-trixie*.ostreeRepo.tar.gz features/ostreeImage/ostree-debian-repo-${dpkg --print-architecture}.tar.gz
+
+REPO=$(find . -name 'ostreeRepo*trixie*.ostreeRepo.tar.gz')
+
+cp $REPO features/ostreeImage/ostree-debian-repo-$(dpkg --print-architecture).tar.gz
+cp $REPO features/ostreeRepo/ostree-debian-repo-$(dpkg --print-architecture).tar.gz
+
 ./build ostreeImage
 rm -rf .build/ostreeRepo*
 echo zsh >> features/ostreeRepo/pkg.include
 ./build ostreeRepo
-mkdir -p .build/repo
-tar xzvf .build/ostreeRepo-${dpkg --print-architecture}-trixie*.ostreeRepo.tar.gz -C .build/repo
+mkdir -p .build/repo/debian-repo-$(dpkg --print-architecture)
+tar xzvf $REPO -C .build/repo/debian-repo-$(dpkg --print-architecture)
+pushd .build/repo/debian-repo-$(dpkg --print-architecture)
+python3 -m http.server 9090 &
+popd
