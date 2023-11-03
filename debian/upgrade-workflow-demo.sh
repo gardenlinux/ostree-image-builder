3079004acf67feafcb0ebaadef1403175ab4ef94
Florian Wilhelm <2292245+fwilhe@users.noreply.github.com>
2023-11-03 14:03:25 +0000

    [no ci] improve demo workflow

--- debian/upgrade-workflow-demo.sh
+++ debian/upgrade-workflow-demo.sh
@@ -1,23 +1,50 @@
 #!/usr/bin/env bash
 
+# Demo for a workflow where the image gets a new package
+# We'll build two OSTree commits, one with the default package list, one with zsh added
+# When booting the image, you'll have the first commit which does not include zsh
+# Once you upgraded to the latest commit, you'll have zsh available
+
 set -eufo pipefail
 
 set -x
 
+HOST_IP=$(hostname -i | awk '{print $3}')
+echo "http://${HOST_IP}:9090" > features/ostreeRepo/CONFIG_REMOTE_URL
+
+ARCH=$(dpkg --print-architecture)
+
+echo "Downloading published repo as a starting point."
+wget --output-document features/ostreeRepo/ostree-debian-repo-${ARCH}.tar.gz http://ostree.gardenlinux.io/ostree-debian-repo-${ARCH}.tar.gz
+
 rm -rf .build
+
+echo "Building new repo with first commit"
 ./build ostreeRepo
 
 REPO=$(find . -name 'ostreeRepo*trixie*.ostreeRepo.tar.gz')
+cp $REPO features/ostreeImage/ostree-debian-repo-${ARCH}.tar.gz
+cp $REPO features/ostreeRepo/ostree-debian-repo-${ARCH}.tar.gz
 
-cp $REPO features/ostreeImage/ostree-debian-repo-$(dpkg --print-architecture).tar.gz
-cp $REPO features/ostreeRepo/ostree-debian-repo-$(dpkg --print-architecture).tar.gz
-
+echo "Building bootable image containing the first commit."
 ./build ostreeImage
 rm -rf .build/ostreeRepo*
+
+echo "Building new repo with second commit that includes zsh."
 echo zsh >> features/ostreeRepo/pkg.include
 ./build ostreeRepo
-mkdir -p .build/repo/debian-repo-$(dpkg --print-architecture)
-tar xzvf $REPO -C .build/repo/debian-repo-$(dpkg --print-architecture)
-pushd .build/repo/debian-repo-$(dpkg --print-architecture)
+
+mkdir -p .build/repo/debian-repo-${ARCH}
+tar xzvf $REPO -C .build/repo/debian-repo-${ARCH}
+pushd .build/repo/
+echo "Serving repo locally so our vm image can be upgraded."
 python3 -m http.server 9090 &
 popd
+
+ostree log --repo=.build/repo/debian-repo-${ARCH} debian/testing/${ARCH} | head -n14
+
+echo "Reverting change which added zsh package"
+git checkout features/ostreeRepo/pkg.include
+
+echo "Done building the demo workflow."
+echo "Next steps: Start the virtual machine, verify the booted system has no zsh available, perform an upgrade, reboot, and verify zsh is available now."
